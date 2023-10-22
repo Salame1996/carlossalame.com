9d89ddbcafee492f8170d5d7e7c0b875ef674432
Carlos Salame <carlosa@Carloss-MacBook-Pro.local>
2023-10-22 16:22:58 -0500

    fixed run build with elint

--- src/app/components/HeroSection.jsx/AboutSection.jsx
+++ src/app/components/HeroSection.jsx/AboutSection.jsx
@@ -14,7 +14,7 @@ const TAB_DATA = [
         <li>CSS</li>
         <li>React.js</li>
         <li>MySQL</li>
-        <li>API's</li>
+        <li>API&apos;s</li>
         <li>Next.js</li>
         <li>Tailwind</li>
         <li>Node</li>
@@ -38,7 +38,7 @@ const TAB_DATA = [
     content: (
       <ul>
         <li>CodeCademy React.js</li>
-        <li>CS50 Harvard X </li>
+        <li>CS50 Harvard X</li>
         <li>FreeCodeCamp JavaScript</li>
       </ul>
     ),
@@ -56,15 +56,15 @@ function AboutSection() {
   };
 
   return (
-    <section id="about" className=" text-white">
+    <section id="about" className="text-white">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
         <Image
           src="/images/pixelcomputer.png"
           width={500}
           height={500}
           alt="Pixel Computer"
-          fallback="/images/pixelcomputer.png" // Provide the path to your fallback image
-        />{" "}
+          fallback="/images/pixelcomputer.png"
+        />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
